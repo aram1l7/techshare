8ec154c508697f2e8cc4cc24d220234b7c868a4d
Aram <arammartirosyann16@gmail.com>
2022-04-15 01:03:08 +0400

    posts api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -20,22 +20,127 @@ router.post(
       });
     }
     try {
-        let user = await User.findById(req.user.id).select("-password");
-        const newPost = new Post({
-          text: req.body.text,
-          name: user.name,
-          avatar: user.avatar,
-          user:req.user.id
-        });
-        const post = await newPost.save();
-        res.json(post);
+      let user = await User.findById(req.user.id).select("-password");
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+      const post = await newPost.save();
+      res.json(post);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Server error");
     }
-    catch(error){
-        console.error(error);
-        res.status(500).send('Server error')
-    }
-
   }
 );
+
+//@route      Post api/posts
+//@desc       get all posts
+//@access     private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Posts.find().sort({
+      date: -1,
+    });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
+
+//@route      Post api/posts/:id
+//@desc       get post by id
+//@access     private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Internal server error");
+  }
+});
+
+
+
+//@route      Put api/posts/:id
+//@desc       edit post by id
+//@access     private
+
+router.put("/:id", auth, async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    post.text=req.body.text;
+    await post.save();
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Internal server error");
+  }
+});
+
+//@route      Post api/posts/:id
+//@desc       delete post by id
+//@access     private
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if(!post){
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Unauthorized" });
+    }
+    await post.remove()
+    res.json({ msg: "Successfully deleted" });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Internal server error");
+  }
+});
+
+//@route      Put api/posts/like/:id
+//@desc       Like a post
+//@access     private
+
+router.put('/like/:id', auth, async (req,res) => {
+  try {
+    const post = await Posts.findById(req.params.id);
+    if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+      let removeIndex = post.likes.map(el => el.user.toString().indexOf(req.user.id));
+      post.likes.splice(removeIndex, 1);
+      await post.save();
+      return res.status(200).json({msg:'Successfully unliked'})
+    }
+    post.likes.unshift({user: req.user.id});
+    await post.save();
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error");
+  }
+})
+
 
 module.exports = router;
