8a420dff7c891da37e639cb4aed8edf3efe57cce
Aram <arammartirosyann16@gmail.com>
2022-04-16 22:33:07 +0400

    backend and client initial structure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './css/index.css'
+function App() {
+  return (
+    <div className="flex m-10 bg-gray-600">
+      <header className="App-header">
+        Hey
+      </header>
+    </div>
+  );
+}
+
+export default App;
