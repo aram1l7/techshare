581dbef845e0c2b6aac4c659994ac2651d2dbc1b
Aram <arammartirosyann16@gmail.com>
2022-04-20 01:41:39 +0400

    landing & register page

--- client/src/components/auth/register/index.js
+++ client/src/components/auth/register/index.js
@@ -1,7 +1,115 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 function Register() {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const { name, email, password, confirmPassword } = formData;
+  const onFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [isPasswordOpen, setIsPasswordOpen] = useState(false);
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+
+  useEffect(() => {
+    if (name.length > 6 && password.length > 6 && confirmPassword.length > 6) {
+      setIsButtonDisabled(false);
+    }
+  }, [formData]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(password !== confirmPassword){
+      return;
+    }
+    
+  };
+
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen">
+      <h2 className="text-2xl mt-5 font-semibold">Sign Up</h2>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        className="form-container mt-10 w-1/2"
+      >
+        <div className="form-input">
+          <input
+            className="w-full border-0 outline-none border-b-2 border-solid p-3"
+            type="text"
+            name="name"
+            value={name}
+            placeholder="Name"
+            onChange={(e) => onFormChange(e)}
+          />
+        </div>
+        <div className="form-input">
+          <input
+            className="w-full border-0 outline-none border-b-2 border-solid p-3"
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => onFormChange(e)}
+          />
+        </div>
+        <div className="form-input flex items-center border-b-2 border-solid">
+          <input
+            className="w-full border-0 outline-none  p-3"
+            type={`${isPasswordOpen ? "text" : "password"}`}
+            name="password"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => onFormChange(e)}
+          />
+          <FontAwesomeIcon
+            className="cursor-pointer"
+            onClick={() => setIsPasswordOpen(!isPasswordOpen)}
+            icon={isPasswordOpen ? faEye : faEyeSlash}
+          />
+        </div>
+        <div className="form-input flex items-center border-b-2 border-solid">
+          <input
+            className="w-full border-0 outline-none  p-3"
+            type={`${isConfirmOpen ? "text" : "password"}`}
+            name="confirmPassword"
+            value={confirmPassword}
+            placeholder="Confirm password"
+            onChange={(e) => onFormChange(e)}
+          />
+          <FontAwesomeIcon
+            className="cursor-pointer"
+            onClick={() => setIsConfirmOpen(!isConfirmOpen)}
+            icon={isConfirmOpen ? faEye : faEyeSlash}
+          />
+        </div>
+        <div className="mt-5">
+          <input
+            type="submit"
+            value="Create account"
+            disabled={isButtonDisabled}
+            className="submit px-4 py-2 rounded-lg bg-green-500 text-white shadow disabled:opacity-50"
+          />
+        </div>
+        <p className="mt-4">
+          Already have an account ?{" "}
+          <Link className="underline" to="/login">
+            Login
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
--- client/src/components/layout/landing/index.jsx
+++ client/src/components/layout/landing/index.jsx
@@ -1,40 +1,43 @@
 import React from "react";
 import Navbar from "../navbar";
-
+import illustration from "images/illustration.png";
+import { Link } from "react-router-dom";
 function Landing() {
   return (
     <>
       <Navbar />
-      <section className="landing flex justify-center items-center">
-        <div className="flex justify-center items-center">
-          <div className="text-center text-white">
-            <h1 className="drop-shadow-md font-bold text-5xl">
-              Developer Connector
-            </h1>
-            <p className="mt-5 text-xl font-medium">
-              DevConnect is a social platform for developers <br /> where people
-              share posts connect with <br /> each other and share with their
-              experience.
-            </p>
-            <p className="text-xl font-medium">
-              Sign up or create an accound if you don't have a one.
-            </p>
+      <section className="landing flex flex-col lg:flex-row items-center justify-center lg:justify-between pl-10">
+        <div className="content">
+          <div className="flex justify-center items-center">
+            <div>
+              <h1 className="drop-shadow-md font-bold text-5xl">
+                DevConnect
+              </h1>
+              <p className="mt-5 text-xl font-medium">
+                DevConnect is a social platform for developers <br /> where
+                people share posts connect with <br /> each other and share with
+                their experience.
+              </p>
+              <p className="text-xl font-medium">
+                Sign up or create an accound if you don't have a one.
+              </p>
 
-            <div className="flex gap-2 justify-center mt-5">
-              <a
-                href="register.html"
-                className="bg-green-500 py-2 px-6 rounded cursor-pointer"
-              >
-                Sign Up
-              </a>
-              <a
-                href="login.html"
-                className="bg-cyan-400 py-2 px-6 rounded cursor-pointer"
-              >
-                Login
-              </a>
+              <div className="mt-10">
+                <Link
+                  to="/register"
+                  className="bg-teal-300 shadow-sm text-white	py-3 font-semibold px-6 rounded cursor-pointer"
+                >
+                  Join now
+                </Link>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="image">
+          <img
+            className="w-full h-full object-cover"
+            src={illustration}
+          />
         </div>
       </section>
     </>
